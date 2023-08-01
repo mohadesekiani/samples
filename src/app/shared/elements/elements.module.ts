141d16b52e0e70b7721e897cef085f2208c1282a
mohadesekiani <mohadeskiani6@gmail.com>
2023-08-01 14:55:01 +0330

    shared

--- src/app/shared/elements/elements.module.ts
+++ src/app/shared/elements/elements.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 //import { DatepickerComponent } from './components/form/datepicker/datepicker.component';
 //import { FlightComponent } from './components/form/flight/flight.component';
 import { PasengersComponent } from './form/fields/pasengers/pasengers.component';
-import { SharedModule } from '../shared.module';
+//import { SharedModule } from '../shared.module';
 import { MaterialModule } from '../material/material.module';
 import { DatepickerComponent } from './form/fields/datepicker/datepicker.component';
 import { FlightComponent } from './form/fields/flight/flight.component';
