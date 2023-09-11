a1e49f5463b75f186be5bc84e4c0087f68b89957
mohadesekiani <mohadesekiani6@gmail.com>
2023-09-11 16:34:00 +0330

    test

--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,6 +1,6 @@
 import { LayoutComponent } from "./layout.component";
 
-describe('SUT: LayoutComponent', () => {
+fdescribe('SUT: LayoutComponent', () => {
   let sut: LayoutComponent;
 
   beforeEach(() => {
@@ -10,4 +10,16 @@ describe('SUT: LayoutComponent', () => {
   it('should create', () => {
     expect(sut).toBeTruthy();
   })
+
+  fit('should be when you click on the tab, the tab becomes active', () => {
+   let tabs = [
+      { title: 'Flight', active: true ,route:'/'},
+      { title: 'Train', active: false ,route:'Train'},
+    ];
+    
+    sut.activateTab(tabs[0])
+    expect(tabs[0].active).toBe(true)
+    expect(tabs[1].active).toBe(false)
+  });
+  
 });
