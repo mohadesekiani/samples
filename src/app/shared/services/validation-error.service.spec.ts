6723f6fa4fcc9e5bde65e2c37d37e17d7215d99e
tiampersian <tiampersian@gmail.com>
2023-10-14 17:27:41 +0330

    10-14

--- src/app/shared/services/validation-error.service.spec.ts
+++ src/app/shared/services/validation-error.service.spec.ts
@@ -14,44 +14,96 @@
 // }
 // }
 //
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+// {
+// messagesDic={
+//     'routes.origin.location':'location is mandatory',
+//     ...
+// }
+// }
+//
+
+// {
+// messagesDic={
+//     'routes.origin.location[0].path':'path is mandatory',
+//     ...
+// }
+// }
+//
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ValidationErrorService } from './validation-error.service';
 
-describe('ValidationErrorService', () => {
-  let sut: ValidationErrorService;
-  beforeEach(() => {
-    sut = new ValidationErrorService();
-  });
-
-  it('should be created', () => {
-    expect(sut).toBeTruthy();
-  });
-
-  it('should get form validation errors', () => {
-    const form = new FormGroup({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl(''),
+fdescribe('ValidationErrorService', () => {
+    let sut: ValidationErrorService;
+    beforeEach(() => {
+        sut = new ValidationErrorService();
     });
 
-    const errors = sut.getFormValidationErrors(form);
-
-    expect(errors).toEqual({
-      firstName: 'The field "firstName" is mandatory.',
+    it('should be created', () => {
+        expect(sut).toBeTruthy();
     });
-  });
 
-  it('should return errors for the given form array', () => {
-    const formArray = new FormGroup({
-      routes: new FormArray([
-        new FormGroup({
-          origin: new FormControl(null, Validators.required),
-        }),
-      ]),
-    });
-    const arrayFormErrors = sut.getFormValidationErrors(formArray);
+    it('should get form validation errors', () => {
+        const form = new FormGroup({
+            firstName: new FormControl('', Validators.required),
+            lastName: new FormControl(''),
+        });
 
-    expect(arrayFormErrors).toEqual({
-      'routes[0].origin': 'Origin is mandatory.',
+        const errors = sut.getFormValidationErrors(form);
+
+        expect(errors).toEqual({
+            firstName: 'The field "firstName" is mandatory.',
+        });
     });
-  });
+
+    fit('should return errors for the given form array', () => {
+        const formArray = new FormGroup({
+            routes: new FormArray([
+                new FormGroup({
+                    origin: new FormControl(null, Validators.required),
+                }),
+            ]),
+        });
+        debugger
+        const arrayFormErrors = sut.getFormValidationErrors(formArray);
+
+        expect(arrayFormErrors).toEqual({
+            'routes[0].origin': 'Origin is mandatory.',
+        });
+    });
+
+    fit('should return errors for the given form array', () => {
+        const formArray = new FormGroup({
+            routes: new FormGroup({
+                origin: new FormGroup({
+                    location: new FormControl(null, Validators.required),
+                }),
+            }),
+        });
+        debugger
+        const arrayFormErrors = sut.getFormValidationErrors(formArray);
+
+        expect(arrayFormErrors).toEqual({
+            'routes[0].origin': 'Origin is mandatory.',
+        });
+    });
+
+    fit('should return errors for the given form array', () => {
+        const formArray = new FormBuilder().group({
+            routes: {
+                origin: new FormGroup({
+                    location: new FormArray([
+                        new FormControl(null, Validators.required),
+                    ])
+                }),
+            },
+        });
+        debugger
+        const arrayFormErrors = sut.getFormValidationErrors(formArray);
+
+        expect(arrayFormErrors).toEqual({
+            'routes[0].origin': 'Origin is mandatory.',
+        });
+    });
 });
--- src/app/shared/services/validation-error.service.ts
+++ src/app/shared/services/validation-error.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AbstractControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
@@ -12,16 +12,30 @@ export class ValidationErrorService {
 
   getFormValidationErrors(form: FormGroup): { [key: string]: string } {
     Object.keys(form.controls).forEach((key) => {
+
+      if ((form.get(key) instanceof FormArray)) {
+        debugger
+        // this.getFormValidationErrors(form.get(key))
+        return;
+      }
+
+      if ((form.get(key) instanceof FormGroup)) {
+
+        return;
+      }
+
       const controlErrors: any = form.get(key)?.errors;
 
       if (!controlErrors) {
         return;
       }
+
       Object.keys(controlErrors).forEach((keyError) => {
         const errorMessage = this.getErrorMessage(key, keyError);
         this.messagesDic[key] = errorMessage;
       });
     });
+
     return this.messagesDic;
   }
 
